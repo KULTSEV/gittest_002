f4aa51fa13b5159c7acbcc73136c885ea4c76bff
KULTSEV <kultsev@mail.ru>
2021-08-31 00:12:35 +0700

    2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,7 +44,7 @@ namespace gittest_002
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(307, 35);
+            this.button2.Location = new System.Drawing.Point(208, 36);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
